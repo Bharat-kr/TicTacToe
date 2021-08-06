707e5ffd8e0042c69e8f85ffb5090c0e6060f0be
Bharat Kumar <kumarbharat661@gmail.com>
2021-08-06 22:41:40 +0530

    displayed game history

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Board from "./components/Board"
 import { calculateWinner } from "./helpers";
+import History from "./components/History";
 import './styles/root.scss'
 
 const App = () => {
@@ -8,7 +9,6 @@ const App = () => {
   const [currentMove, setCurrentMove] = useState(0);
 
   const current = history[currentMove];
-
   const winner = calculateWinner(current.board);
   const message = winner ? `Winner is ${winner}`: `Next player is ${current.isXNext ? 'X':'O'}`;
 
@@ -28,14 +28,20 @@ const App = () => {
           })
           return prev.concat({board:newBoard, isXNext: !last.isXNext})
       });
+      
       setCurrentMove(prev => prev + 1);
   };
+
+  const moveTo = move => {
+    setCurrentMove(move);
+  }
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
